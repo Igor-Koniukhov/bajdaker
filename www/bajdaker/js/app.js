151a0b9e0c7d8e1a1cf6a6f9dec0232520f0dfda
Igor-Koniukhov <ikoniukhov@gmail.com>
2021-07-06 21:18:26 +0300

    added last changes

--- www/bajdaker/js/app.js
+++ www/bajdaker/js/app.js
@@ -1,11 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
+/*//mutation observer snippet---------------
+	const targetEl = document.querySelector('body');
+
+	function callback(mutations, observer) {
+		console.log(mutations);
+	}
+
+	const observer = new MutationObserver(callback);
+	observer.observe(targetEl, {
+		childList: true,
+		attributes: true,
+		characterData: true,
+		subtree: true,
+		attributeFilter: [""],
+		characterDataOldValue: true
+	});
 
 
-	let nIndex = 3;
+//mutation observer snippet---------------*/
 
 
-	let nav = document.querySelector('.nav'),
-		navLine = document.querySelector('.nav__line'),
+	let navLine = document.querySelector('.nav__line'),
 		navItem = document.querySelectorAll('.nav__item');
 	let navLineFooter = document.querySelector('.nav__line-footer'),
 		navItemFooter = document.querySelectorAll('.nav__item-footer');
@@ -57,46 +72,55 @@ document.addEventListener("DOMContentLoaded", function () {
 		$(this).parent().children().fadeOut();
 	});
 
+	let articleTitle = document.querySelector('h2');
+	let articleTitleHeight = articleTitle.getBoundingClientRect().top;
+	let topLine = document.querySelector('.top-line');
+	let heightTopLine = topLine.getBoundingClientRect().height;
+	let TopTopLine = topLine.getBoundingClientRect().top;
+	let navPageTop = document.querySelector('.tabs');
+	let scrollUpButton = document.querySelector('.roll-up');
+//snippet which prevent glue with the header on reload
+
+	window.addEventListener("beforeunload", function (event) {
+		console.log("All resources finished loading!");
+		window.scrollTo(0, 0);
+	});
 
 
-
-
-	$(window).on('scroll', function () {
-		let topLine = document.querySelector('.top-line');
-		let heightTopLine = topLine.clientHeight;
-		let heightBlock = document.querySelector('.first-position-header');
-		let heightBlockTop = heightBlock.clientHeight;
+	window.addEventListener('scroll', function (event) {
 		let logo = document.querySelector('.nav-wrapper_logo');
-		let heightLogo = logo.clientHeight;
-		console.log(heightLogo);
+		let scrollUpButton = document.querySelector('.roll-up');
+		let scrollUpButtonBottom = scrollUpButton.getBoundingClientRect().bottom;
 
-
-		if ($(this).scrollTop() >= heightBlockTop) {
+		let footer = document.querySelector('footer');
+		let footerRedPoint = footer.getBoundingClientRect().top;
+		if ($(this).scrollTop() >= TopTopLine) {
 			$(topLine).addClass('fixed');
+			$(scrollUpButton).css({'display': 'block'});
+
 			$(logo).css({'margin-top': heightTopLine + 'px'});
 		} else {
 			$(topLine).removeClass('fixed');
 			$(logo).css({'margin-top': 0});
+			$(scrollUpButton).css({'display': 'none'});
 		}
-
-		let navPageTop = document.querySelector('.tabs')
-		let heightTitle = document.querySelector('.post-title').clientHeight;
-		let heightQuote = document.querySelector('.post-quote').clientHeight;
-		let heightBreadCrumbs = document.querySelector('.breadcrumbs').clientHeight;
-		var heightBlockSum = (heightBlockTop + heightTopLine
-			+ heightTitle + heightQuote + heightBreadCrumbs
-			+ heightLogo + 23);
-
-
-		if ($(this).scrollTop() >= heightBlockSum) {
+		let navPageTop = document.querySelector('.tabs');
+		if ($(this).scrollTop() >= articleTitleHeight) {
 			$(navPageTop).addClass('fixed-nav');
 		} else {
 			$(navPageTop).removeClass('fixed-nav');
 		}
 
+		if (footerRedPoint <= scrollUpButtonBottom) {
+			scrollUpButton.style.backgroundColor = 'red';
+
+
+		} else {
+			scrollUpButton.style.backgroundColor = 'unset';
+		}
+
 
 	});
-
 
 	function sliderLoadFunction() {
 		let swiper = new Swiper('.swiper-container_custom', {
@@ -121,18 +145,17 @@ document.addEventListener("DOMContentLoaded", function () {
 	};
 
 
-//tab in the post trips about rever Vorscla
+//tab in the post trips about river Vorscla
 	$('.tab').click(function () {
-
-
-		let tabChildren = $('.tab').children();
+		let tb = $('.tab');
+		let tabChildren = $(tb).children();
 		let lastTabChild = $('.tab:last-child');
 		let tabPrev = $(this).prev();
 		let tabNext = $(this).next();
 		let tabFirstChild = $('.tab:first-child');
 		let num = $(this).index();
 		$(tabFirstChild).css({'border-left': '1px solid #333', 'border-radius': '5px 0 0 0'})
-		$('.tab').removeClass('tab-active')
+		$(tb).removeClass('tab-active')
 			.css({
 				'border-left': 'unset',
 				'border-right': 'unset',
@@ -156,16 +179,95 @@ document.addEventListener("DOMContentLoaded", function () {
 		sliderLoadFunction();
 	});
 
-	$(".tab").click(function() {
-		$("html, body").animate({ scrollTop: 0 }, "slow");
-		return false;
-	});
+
+	let scrollUpFunction = (but) => {
+		$(but).click(function () {
+			$("html, body").animate({scrollTop: 0}, "slow");
+			return false;
+		});
+	}
+	scrollUpFunction('.tab');
+	scrollUpFunction(scrollUpButton);
+
 
 // swipers always at the end--------
 	sliderLoadFunction();
+	let windowWidth = window.outerWidth;
+	console.log(windowWidth);
 
+
+	$('.img-responsiv').dblclick(function () {
+		if (windowWidth >= 576) {
+			$(this).toggleClass('img-fixed');
+		}
+
+	});
+
+
+	$(function () {
+
+		document.getElementById('ajax-contact-form').addEventListener('submit', function (evt) {
+			var http = new XMLHttpRequest(), f = this;
+			var th = $(this);
+			evt.preventDefault();
+			http.open("POST", "contact.php", true);
+			http.onreadystatechange = function () {
+				if (http.readyState == 4 && http.status == 200) {
+					alert(http.responseText);
+					if (http.responseText.indexOf(f.nameFF.value) == 0) { // очистить поля формы, если в ответе первым словом будет имя отправителя (nameFF)
+						th.trigger("reset");
+					}
+				}
+			}
+			http.onerror = function () {
+				alert('Ошибка, попробуйте еще раз');
+			}
+			http.send(new FormData(f));
+		}, false);
+
+	});
+
+	let ownerEmail = $('.owner-email').html();
+
+
+	let mainColor = '#0e2257';
+	let trigger = document.querySelector('.trigger');
+	$(trigger).on('click', function () {
+		$('.form-wrapper').removeClass('d-none');
+		$('.main-page-wrapper').addClass('d-none');
+		$('.trigger-wrapper').addClass('d-none');
+		$('body').css({'background-color': mainColor});
+		$('.article-content').addClass('d-none');
+
+		$('#contactOwner').val(ownerEmail);
+	})
+	$('.cancel').on('click', function () {
+		$('.form-wrapper').addClass('d-none');
+		$('.main-page-wrapper').removeClass('d-none');
+		$('.trigger-wrapper').removeClass('d-none');
+		$('body').css({'background-color': 'initial'});
+		$('.article-content').removeClass('d-none');
+	})
+ if($('body').hasClass('is-category')){
+ 	$('video').css({'height':'200px'});
+ }
 
 });
+
+let url ={
+	link: ["https://drive.google.com/file/d/1rfwf6Ptri5jMSXCSSKNyjSxX_BUzDWJv/view?usp=sharing",
+		"https://drive.google.com/file/d/15Oyecs5nZEonB4t24sr1sJyx3VRe4r3w/view?usp=sharing"],
+
+	openNewWindow(x) {
+		document.getElementsByClassName('open-new-window')[x].onclick =  () => {
+			window.open(this.link[x]);
+
+		}
+	},
+}
+
+
+
 
 
 
